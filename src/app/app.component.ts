d5dfc6ff0fd45207a9d1e51b108ab9fb0310ddfe
Mohamed Mseddi <mameya.mseddi@gmail.com>
2020-04-01 01:32:35 +0100

    switched to angular material and canvasjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {CovidService} from './services/covid.service';
 import {Result} from './models/result';
 import {World} from './models/world';
 import {ChartData} from './models/chart-data';
 import {concatMap, expand} from 'rxjs/operators';
 import {timer} from 'rxjs';
+import * as CanvasJS from './canvasjs.min.js';
+import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -21,44 +23,45 @@ export class AppComponent implements OnInit {
   public todayCases: Result[];
   public data: ChartData;
   public top10: Result[];
-  public repeateRate=20;
-  public options:any;
+  public repeateRate = 20;
+  public options: any;
+  public dataSource: MatTableDataSource<Result>;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor(private covidService: CovidService) {
   }
 
   ngOnInit(): void {
+
     this.options = {
       legend: {
-        display:false
+        display: false
       }
     };
-    this.cols = [
-      {field: 'country', header: 'Country'},
-      {field: 'cases', header: 'Cases'},
-      {field: 'deaths', header: 'Deaths'},
-      {field: 'recovered', header: 'Recovered'},
-      {field: 'todayDeaths', header: 'Today Deaths'},
-      {field: 'todayCases', header: 'Today Cases'},
-    ];
+    this.cols = ['country', 'cases', 'deaths', 'recovered', 'todayDeaths','todayCases',];
     this.covidService.getWorldData().pipe(
-      expand(_ => timer(this.repeateRate*1000).pipe(concatMap(_ => this.covidService.getWorldData())))).subscribe((global) => {
+      expand(_ => timer(this.repeateRate * 1000).pipe(concatMap(_ => this.covidService.getWorldData())))).subscribe((global) => {
       this.global = global;
     });
-    this.covidService.getAllData().pipe(
-      expand(_ => timer(this.repeateRate*1000).pipe(concatMap(_ => this.covidService.getAllData())))).subscribe((data) => {
-      this.results = data;
-      this.top10 = data.slice(0, 10);
-      this.data = new ChartData(this.top10, 'Top 10 cases');
-
-    });
     this.covidService.getAllDataByTodayDeaths().pipe(
-      expand(_ => timer(this.repeateRate*1000).pipe(concatMap(_ => this.covidService.getAllDataByTodayDeaths())))).subscribe((data) => {
+      expand(_ => timer(this.repeateRate * 1000).pipe(concatMap(_ => this.covidService.getAllDataByTodayDeaths())))).subscribe((data) => {
       this.todayDeaths = data.slice(0, 10);
     });
     this.covidService.getAllDataByTodayCases().pipe(
-      expand(_ => timer(this.repeateRate*1000).pipe(concatMap(_ => this.covidService.getAllDataByTodayCases())))).subscribe((data) => {
+      expand(_ => timer(this.repeateRate * 1000).pipe(concatMap(_ => this.covidService.getAllDataByTodayCases())))).subscribe((data) => {
       this.todayCases = data.slice(0, 10);
+    });
+    this.covidService.getAllData().pipe(
+      expand(_ => timer(this.repeateRate * 1000).pipe(concatMap(_ => this.covidService.getAllData())))).subscribe((data) => {
+      this.results = data;
+      this.dataSource = new MatTableDataSource(this.results);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      this.top10 = data.slice(0, 10);
+      this.data = new ChartData(this.top10, 'Top 10 cases');
+      this.loadChart();
+
     });
   }
 
@@ -66,4 +69,40 @@ export class AppComponent implements OnInit {
     return this.global != null && this.results != null && this.todayDeaths != null && this.todayCases != null;
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  loadChart() {
+    let chart = new CanvasJS.Chart('chartContainer', {
+      animationEnabled: true,
+      theme: 'dark1',
+      backgroundColor: 'black',
+      title: {
+        text: 'TOP 10 Countries',
+      },
+      data: [{
+        startAngle: 240,
+        indexLabel: '{label} {y}',
+        type: 'pie',
+        dataPoints: this.top10.map((top) => new DataPoint(top.cases, top.country))
+      }]
+    });
+
+    chart.render();
+  }
+
+}
+
+export class DataPoint {
+  constructor(
+    public y: number,
+    public label: string
+  ) {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule} from '@angular/common/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ChartModule} from 'primeng/chart';
-import {ProgressSpinnerModule, TableModule} from 'primeng';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule
+} from '@angular/material';
+import { StatsComponent } from './stats/stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    ChartModule,
-    TableModule,
-    ProgressSpinnerModule
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
 
   ],
   providers: [],
